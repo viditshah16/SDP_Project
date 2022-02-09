65814fe17fcf5bab4a10142ef94de1fec80ae4cb
Bhavin Chavda <bhavinchavda096@gmail.com>
2022-02-09 23:09:23 +0530

    Now User can Create Room and chat in different Rooms also admin of room can edit room like changing roomname , add or remove room members

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from logging import exception
 from flask import Flask, redirect, render_template, request, url_for, redirect
 from flask_socketio import SocketIO , join_room
-from db import get_user, save_user
+from db import add_room_member, add_room_members, get_room_members, get_rooms_for_user, get_user, is_room_admin, is_room_member, save_room, save_user , get_room , update_room , remove_room_members
 from video import livevideo 
 import cv2
 from pymongo.errors import DuplicateKeyError
@@ -19,7 +19,11 @@ login_manager.init_app(app)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    rooms = []
+    if current_user.is_authenticated:
+        rooms=get_rooms_for_user(current_user.username)
+
+    return render_template("index.html" , rooms=rooms)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -65,34 +69,62 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/chat' , methods=['GET' , 'POST'])
+@app.route('/create-room/' , methods=['GET' , 'POST'])
 @login_required
-def chat():
-    if request.method == "POST":
-        username = request.form["username"]
-        room = request.form["room"]
+def create_room():
+    message = ''
+    if request.method == 'POST':
+        room_name = request.form.get('room_name')
+        usernames = [username.strip() for username in request.form.get('members').split(',')]
 
-    if username and room:
-        return render_template('chat.html' , username=username , room=room)
+        if len(room_name) and len(usernames):
+            room_id = save_room(room_name, current_user.username)
+            if current_user.username in usernames:
+                usernames.remove(current_user.username)
+            add_room_members(room_id, room_name, usernames, current_user.username)
+            return redirect(url_for('view_room', room_id=room_id))
+        else:
+            message = "Failed to create room"
+    return render_template('create_room.html', message=message)
+
+@app.route('/rooms/<room_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_room(room_id):
+    room = get_room(room_id)
+    if room and is_room_admin(room_id, current_user.username):
+        existing_room_members = [member['_id']['username'] for member in get_room_members(room_id)]
+        room_members_str = ",".join(existing_room_members)
+        message = ''
+        if request.method == 'POST':
+            room_name = request.form.get('room_name')
+            room['name'] = room_name
+            update_room(room_id, room_name)
+
+            new_members = [username.strip() for username in request.form.get('members').split(',')]
+            members_to_add = list(set(new_members) - set(existing_room_members))
+            members_to_remove = list(set(existing_room_members) - set(new_members))
+            if len(members_to_add):
+                add_room_members(room_id, room_name, members_to_add, current_user.username)
+            if len(members_to_remove):
+                remove_room_members(room_id, members_to_remove)
+            message = 'Room edited successfully'
+            room_members_str = ",".join(new_members)
+        return render_template('edit_room.html', room=room, room_members_str=room_members_str, message=message)
+    else:
+        return "Room not found", 404
+
+
+@app.route('/rooms/<room_id>/')
+@login_required
+def view_room(room_id):
+    print(room_id)
+    room = get_room(room_id)
+    print(room)
+    if room and is_room_member(room_id , current_user.username):
+        room_members = get_room_members(room_id)
+        return render_template('view_room.html', username=current_user.username, room=room, room_members=room_members)
     else: 
-        return redirect(url_for('home'))
-
-
-
-# @app.route('/after' , methods=['GET' , 'POST'])
-# def after():
-#     image  = livevideo()
-#     img = cv2.imread(image , 0)
-
-#     img = cv2.resize(img , (48,48))
-#     img = np.reshape(img ,(1,48,48,1))
-    
-#     model = load_model('emotion_model.h5')
-#     prediction = model.predict(img)
-#     label_map =['Anger' , 'Neutral' , 'Fear' , 'Happy' , 'Sad' , 'Surprise']
-#     prediction = np.argmax(prediction)
-#     final = label_map[prediction]
-#     return render_template('after.html' , data=final)
+        return "Room Not Found" , 404
 
 
 @socketio.on('join_room')
